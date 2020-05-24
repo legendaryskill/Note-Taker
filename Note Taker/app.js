36192387b1f56a419127c2115f7acb05cdca49a4
RJC Desktop <reuben.crocker@gmail.com>
2020-05-24 15:37:56 +0800

    adding entire working note taker app

---
+++ Note Taker/app.js
@@ -0,0 +1,74 @@
+// importing packages
+const express = require("express");
+const fs =  require("fs");
+const app = express();
+const path = require("path")
+const { v4: uuidv4 } = require('uuid');
+
+// setting up the port
+const PORT = 8080
+
+const db_path = path.join(__dirname, '/db/db.json')
+
+// static files
+app.use(express.static('public'))
+app.use(express.urlencoded({extended: true}))
+
+app.get('/', (req, res) => {
+    let html = fs.readFileSync(path.join(__dirname , "/public/index.html"), "utf-8")
+    res.send(html)
+})
+
+app.get('/notes', (req, res) => {
+    // let html = fs.readFileSync(__dirname + "./public/notes.html", "utf-8")
+    res.sendFile(path.join(__dirname , "/public/notes.html"))
+})
+
+app.get(`/api/notes`, (req, res) => {
+    let notes = JSON.parse(fs.readFileSync(db_path))
+    console.log(notes);
+    res.json(notes)
+})
+
+app.post(`/api/notes`, (req, res) => {
+
+    // let obj = {}
+
+    // obj.id = uuidv4();
+
+    // receive a new note (user input via form)
+    let newNote = req.body
+
+    newNote.id = uuidv4();
+    // add it to the db.json file in object format
+    let allNotes = JSON.parse(fs.readFileSync(db_path))
+    allNotes.push(newNote)
+    // json -- string looks like array/obj
+    // stringify allNotes and update db.json file
+    let updatedNotes = JSON.stringify(allNotes)
+    fs.writeFileSync(db_path, updatedNotes)
+
+    // return the newest note
+    res.json(newNote)
+})
+
+app.delete(`/api/notes/:id`, (req, res) => {
+    // read the allNotes object
+    let allNotes = JSON.parse(fs.readFileSync(db_path))
+    // identify id of the note that is linked to the trash can button on the html page
+    const id = req.params.id;
+    // exclude that object from the allNotes array
+    let notesToKeep = allNotes.filter(function(note){
+        return note.id !== id
+    })
+    // save the updated array to db.json
+    let updatedNotes = JSON.stringify(notesToKeep)
+    fs.writeFileSync(db_path, updatedNotes)
+
+    res.json({success: true})
+})
+
+app.listen(PORT, function(){
+    console.log(`Note Taker app listening at http://localhost:${PORT}`);
+});
+
--- README.md
+++ README.md
@@ -1,2 +1,26 @@
-# Note-Taker
-An app for taking and deleting apps
+<h1 align="center">Welcome to Note-Taker 👋</h1>
+<p>
+  <a href="https://github.com/legendaryskill/Note-Taker" target="_blank">
+    <img alt="Documentation" src="https://img.shields.io/badge/documentation-yes-brightgreen.svg" />
+  </a>
+</p>
+
+>Note Taker is a web-app that can be used to add and delete notes, via a form. Very handy for those who are looking to organise themselves in a structured way.
+
+>It is built on javascript and utilises some middleware such as express
+
+### 🏠 [Homepage](https://legendaryskill.github.io/Note-Taker/)
+
+## Author
+
+👤 **Reuben C.**
+
+* Github: [@legendaryskill](https://github.com/legendaryskill)
+* LinkedIn: [@https:\/\/www.linkedin.com\/in\/blockchain-crypto-investor\/](https://linkedin.com/in/https:\/\/www.linkedin.com\/in\/blockchain-crypto-investor\/)
+
+## Show your support
+
+Give a ⭐️ if this project helped you!
+
+***
+_This README was generated with ❤️ by [readme-md-generator](https://github.com/kefranabg/readme-md-generator)_
